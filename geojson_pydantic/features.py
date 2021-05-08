b25c296dc3a49b412840486e5691d60f6b5c2caa
Benjamin Ramser <legionaerr@googlemail.com>
2021-05-08 12:17:40 +0200

    feat: add generics to feature properties

--- geojson_pydantic/features.py
+++ geojson_pydantic/features.py
@@ -1,19 +1,22 @@
 """pydantic models for GeoJSON Feature objects."""
 
-from typing import Any, Dict, List, Optional
+from typing import List, Optional, Generic, TypeVar
 
-from pydantic import BaseModel, Field, validator
+from pydantic import Field, validator
+from pydantic.generics import GenericModel
 
 from .geometries import Geometry
 from .utils import BBox
 
+T = TypeVar("T")
 
-class Feature(BaseModel):
+
+class Feature(GenericModel, Generic[T]):
     """Feature Model"""
 
     type: str = Field("Feature", const=True)
     geometry: Geometry
-    properties: Optional[Dict[Any, Any]]
+    properties: Optional[T]
     id: Optional[str]
     bbox: Optional[BBox]
 
@@ -30,11 +33,11 @@ class Feature(BaseModel):
         return v
 
 
-class FeatureCollection(BaseModel):
+class FeatureCollection(GenericModel, Generic[T]):
     """FeatureCollection Model"""
 
     type: str = Field("FeatureCollection", const=True)
-    features: List[Feature]
+    features: List[Feature[T]]
     bbox: Optional[BBox]
 
     def __iter__(self):
--- test/test_features.py
+++ test/test_features.py
@@ -1,14 +1,59 @@
 import pytest
+from pydantic import BaseModel
+from uuid import uuid4
+from random import randint
 
 from geojson_pydantic.features import Feature, FeatureCollection
-from geojson_pydantic.geometries import Point
 
 
-@pytest.mark.parametrize("coordinates", [(1, 2)])
-def test_geometry_collection_iteration(coordinates):
+class GenericProperties(BaseModel):
+    id: str
+    description: str
+    size: int
+
+
+properties = {
+    "id": str(uuid4()),
+    "description": str(uuid4()),
+    "size": randint(0, 1000),
+}
+
+polygon = {
+    "type": "Polygon",
+    "coordinates": [
+        [
+            [13.38272, 52.46385],
+            [13.42786, 52.46385],
+            [13.42786, 52.48445],
+            [13.38272, 52.48445],
+            [13.38272, 52.46385],
+        ]
+    ],
+}
+
+test_feature = {
+    "type": "Feature",
+    "geometry": polygon,
+    "properties": properties,
+}
+
+
+def test_geometry_collection_iteration():
     """test if feature collection is iterable"""
-    print(coordinates, type(coordinates))
-    polygon = Point(coordinates=coordinates)
-    feature = Feature(geometry=polygon)
-    gc = FeatureCollection(features=[feature, feature])
+    gc = FeatureCollection(features=[test_feature, test_feature])
     iter(gc)
+
+
+def test_generic_properties_is_dict():
+    feature = Feature(**test_feature)
+    assert feature.properties["id"] == test_feature["properties"]["id"]
+    assert type(feature.properties) == dict
+    assert not hasattr(feature.properties, "id")
+
+
+def test_generic_properties_is_object():
+    feature = Feature[GenericProperties](**test_feature)
+
+    assert feature.properties.id == test_feature["properties"]["id"]
+    assert type(feature.properties) == GenericProperties
+    assert hasattr(feature.properties, "id")
