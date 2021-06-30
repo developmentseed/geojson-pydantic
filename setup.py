92f4d4a40ff8441460c4a767bcdf4168fc7502cd
Regis Brilhante <regis.f.brilhante@gmail.com>
2021-06-30 17:31:59 -0300

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ extra_reqs = {
 
 setup(
     name="geojson-pydantic",
-    version="0.3.0",
+    version="0.3.1",
     python_requires=">=3.7",
     description=u"""Pydantic data models for the GeoJSON spec""",
     long_description=readme,
