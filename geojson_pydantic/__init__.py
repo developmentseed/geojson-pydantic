d32e17e68fab70bef2a8d7991e1688ad28e7a450
David Farrington <david@shipit.ltd>
2022-03-19 06:53:53 +0000

    Declaring public objects via __all__

--- geojson_pydantic/__init__.py
+++ geojson_pydantic/__init__.py
@@ -12,3 +12,15 @@ from .geometries import (  # noqa
 )
 
 version = "0.3.3"
+
+__all__ = [
+    'Feature',
+    'FeatureCollection',
+    'GeometryCollection',
+    'LineString',
+    'MultiLineString',
+    'MultiPoint',
+    'MultiPolygon',
+    'Point',
+    'Polygon'
+]
