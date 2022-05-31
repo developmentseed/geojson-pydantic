be2661d372abc23ff277880f61474235ed40f659
Jeffrey Vervoort <jeffrey.vervoort@knmi.nl>
2022-05-31 09:03:15 +0200

    To have not the same configuration in multiple places remove it from setup.py so that it only exists in setup.cfg.

--- setup.py
+++ setup.py
@@ -1,41 +1,6 @@
-"""Setup for geojson-pydantic."""
+"""Setup for geojson-pydantic.
+    For the configuration of the metadata and options see setup.cfg."""
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-with open("README.md") as f:
-    readme = f.read()
-
-# Runtime requirements.
-inst_reqs = ["pydantic"]
-
-extra_reqs = {
-    "test": ["pytest", "pytest-cov"],
-    "dev": ["pre-commit"],
-}
-
-setup(
-    name="geojson-pydantic",
-    python_requires=">=3.7",
-    description="""Pydantic data models for the GeoJSON spec""",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    classifiers=[
-        "Intended Audience :: Information Technology",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.7",
-        "Topic :: Scientific/Engineering :: GIS",
-    ],
-    keywords="geojson pydantic",
-    author="Drew Bollinger",
-    author_email="drew@developmentseed.org",
-    url="https://github.com/developmentseed/geojson-pydantic",
-    license="MIT",
-    packages=find_packages(exclude=["tests"]),
-    install_requires=inst_reqs,
-    extras_require=extra_reqs,
-    package_data={"geojson_pydantic": ["*.typed"]},
-)
+if __name__ == "__main__":
+    setup()
