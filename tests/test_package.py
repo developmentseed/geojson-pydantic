78b02404e1738e12ad9b8ca35e7049a5146ebcbd
vincentsarago <vincent.sarago@gmail.com>
2022-06-03 00:52:07 +0200

    add sorted and update changelog

--- tests/test_package.py
+++ tests/test_package.py
@@ -3,7 +3,7 @@ import geojson_pydantic
 
 def test_import_namespace():
     """We have exposed all of the public objects via __all__"""
-    assert geojson_pydantic.__all__ == [
+    assert sorted(geojson_pydantic.__all__) == [
         "Feature",
         "FeatureCollection",
         "GeometryCollection",
