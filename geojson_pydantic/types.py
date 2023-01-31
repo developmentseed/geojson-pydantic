40952c45efac05e9f56d8dfcb3c8210f47aa53db
Erik Seglem <erik.seglem@gmail.com>
2023-01-31 13:26:40 -0500

    Move some types outside of TYPE_CHECKING.

--- geojson_pydantic/types.py
+++ geojson_pydantic/types.py
@@ -11,18 +11,14 @@ BBox = Union[
 Position = Union[Tuple[float, float], Tuple[float, float, float]]
 
 # Coordinate arrays
-
 if TYPE_CHECKING:
-    MultiPointCoords = List[Position]
     LineStringCoords = List[Position]
     LinearRing = List[Position]
-    MultiLineStringCoords = List[List[Position]]
-    PolygonCoords = List[List[Position]]
-    MultiPolygonCoords = List[List[List[Position]]]
 else:
-    MultiPointCoords = conlist(Position)
     LineStringCoords = conlist(Position, min_items=2)
     LinearRing = conlist(Position, min_items=4)
-    MultiLineStringCoords = conlist(LineStringCoords)
-    PolygonCoords = conlist(LinearRing)
-    MultiPolygonCoords = conlist(PolygonCoords)
+
+MultiPointCoords = List[Position]
+MultiLineStringCoords = List[LineStringCoords]
+PolygonCoords = List[LinearRing]
+MultiPolygonCoords = List[PolygonCoords]
