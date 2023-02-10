d397a6e36eff2adf8ccf30c866dac64cf585ff92
Erik Seglem <erik.seglem@gmail.com>
2023-02-10 08:32:46 -0500

    Privatize the Protocol.

--- geojson_pydantic/geo_interface.py
+++ geojson_pydantic/geo_interface.py
@@ -3,7 +3,7 @@
 from typing import Any, Dict, Protocol
 
 
-class DictProtocol(Protocol):
+class _DictProtocol(Protocol):
     """Protocol for use as the type of self in the mixin."""
 
     def dict(self, *, exclude_unset: bool, **args: Any) -> Dict[str, Any]:
@@ -15,7 +15,7 @@ class GeoInterfaceMixin:
     """Mixin for __geo_interface__ on GeoJSON objects."""
 
     @property
-    def __geo_interface__(self: DictProtocol) -> Dict[str, Any]:
+    def __geo_interface__(self: _DictProtocol) -> Dict[str, Any]:
         """GeoJSON-like protocol for geo-spatial (GIS) vector data.
 
         ref: https://gist.github.com/sgillies/2217756#__geo_interface
