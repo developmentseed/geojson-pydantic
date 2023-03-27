ba97083473cb8cea9315c0a0afed210e101b3e57
Nathan Zimmerman <npzimmerman@gmail.com>
2023-03-27 10:25:16 -0500

    Add validation for order of bounding boxes

--- geojson_pydantic/features.py
+++ geojson_pydantic/features.py
@@ -27,6 +27,19 @@ class Feature(GenericModel, Generic[Geom, Props], GeoInterfaceMixin):
 
         use_enum_values = True
 
+    @validator("bbox", pre=True)
+    def check_bbox(cls, bbox: BBox) -> BBox:
+        if len(bbox) == 6:
+            if bbox[0] > bbox[3] or bbox[1] > bbox[4] or bbox[2] > bbox[5]:
+                raise ValueError("BBox must be in the form [west, south, bottom, east, north, top]")
+        elif len(bbox) == 4:
+            if bbox[0] > bbox[2] or bbox[1] > bbox[3]:
+                raise ValueError("BBox must be in the form [west, south, east, north]")
+        else:
+            raise ValueError("BBox must be in the form [west, south, east, north] or [west, south, bottom, east, north, top]")
+        return bbox 
+
+
     @validator("geometry", pre=True, always=True)
     def set_geometry(cls, geometry: Any) -> Any:
         """set geometry from geo interface or input"""
